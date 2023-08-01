4bd70b4c7808b938eff3e0e32ba37802d0aafc95
youmin <Merryoumin@gmail.com>
2023-08-01 20:16:02 +0900

    main name and  pic 수정

--- front/src/pages/Main.jsx
+++ front/src/pages/Main.jsx
@@ -8,64 +8,70 @@ import NftBox from "../components/Nftbox";
 const Main = () => {
   const { account, setAccount } = useContext(AppContext);
   const [isModalOn, setIsModalOn] = useState(false);
-  const [data , setData ] = useState() ;
+  const [data, setData] = useState();
   const [searchParams, setSearchParams] = useSearchParams();
   const address = searchParams.get("address");
-  const [level,setLevel ] = useState();
+  const [level, setLevel] = useState();
 
   const onClickModal = () => {
     setIsModalOn(!isModalOn);
   };
-  
+
   const get_nft_data = async () => {
-
     try {
+      const response = await axios.get(
+        `${process.env.REACT_APP_BACKEND_URL}/nft/${account.address}`,
+        {
+          headers: {
+            "ngrok-skip-browser-warning": "any",
+          },
+        }
+      );
 
-        const response = await axios.get(
-          `${process.env.REACT_APP_BACKEND_URL}/nft/${account.address}`,
-          {
-            headers: {
-              "ngrok-skip-browser-warning": "any",
-            },
-          }
+      setData(response.data);
+      console.log(response.data);
+      if (response.data.length < 3) {
+        setLevel(
+          `https://github.com/arypte/swf_hackathon_project/blob/main/Images/Emblem/Black.png?raw=true`
         );
-
-        setData(response.data);
-        console.log( response.data ) ;
-        if( response.data.length < 3 ){
-          setLevel( `https://github.com/arypte/swf_hackathon_project/blob/main/Images/Emblem/Black.png?raw=true` ) ;
-        }
-        else if( response.data.length < 5 ) {
-          setLevel( `https://github.com/arypte/swf_hackathon_project/blob/main/Images/Emblem/Silver.png?raw=true` ) ;
-        }
-        else{
-          setLevel( `https://github.com/arypte/swf_hackathon_project/blob/main/Images/Emblem/Gold.png?raw=true` ) ;
-        }
-
-    } 
-    catch (error) {
+      } else if (response.data.length < 5) {
+        setLevel(
+          `https://github.com/arypte/swf_hackathon_project/blob/main/Images/Emblem/Silver.png?raw=true`
+        );
+      } else {
+        setLevel(
+          `https://github.com/arypte/swf_hackathon_project/blob/main/Images/Emblem/Gold.png?raw=true`
+        );
+      }
+    } catch (error) {
       console.error(error);
     }
-
   };
 
   useEffect(() => {
     // console.log(account) ;
-    get_nft_data() ;
+    get_nft_data();
   }, []);
 
   return (
     <div className="h-full mb-24 overflow-y-scroll overflow-x-hidden scrollbar-hide">
-      <div className="flex justify-around mt-16 border-b-4 pb-12 border-zinc-200">
-        <div className="font-bold">
-          <div className="text-2xl">
+      <div
+        className="flex justify-around   h-44 items-center  border-b-4
+       border-zinc-200"
+      >
+        <div>
+          <div className="text-2xl font-bold">
             {account ? `${account.name},` : "User Name,"}
           </div>
           <div>Hi, Haechi, traveler!</div>
         </div>
-        <div className="w-[70px] h-[70px] bg-neutral-500 rounded-3xl ml-4 relative">
-          
-          { level  && <img src= {`${level}`} ></img> }
+        <div className="w-[70px] h-[70px]  bg-neutral-500 rounded-3xl ml-4 relative">
+          {level && (
+            <img
+              className="justify-center items-center flex"
+              src={`${level}`}
+            ></img>
+          )}
           {/* <button
             onClick={onClickModal}
             className="absolute top-[-20px] right-[-20px] w-10 h-10 bg-neutral-500 rounded-3xl"
@@ -85,9 +91,9 @@ const Main = () => {
         </div>
         <div className="flex justify-center items-center mt-6 mb-6">
           <div className="grid grid-cols-2 gap-x-5 gap-y-6">
-            { data?.map( (v,i) => {
-              return <NftBox key={i} idx={v.tokenID} /> 
-            } )}
+            {data?.map((v, i) => {
+              return <NftBox key={i} idx={v.tokenID} />;
+            })}
           </div>
         </div>
       </div>
